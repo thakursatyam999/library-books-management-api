c495a0d325107a999f6e29f24991218987181e39
Satyam <satyam.thakur@harness.io>
2026-08-17 12:30:32 +0530

    Intentionally broke BookController

--- src/main/java/net/satyamthakur/librarybooksmanagementapi/controller/BookController.java
+++ src/main/java/net/satyamthakur/librarybooksmanagementapi/controller/BookController.java
@@ -39,7 +39,7 @@ public class BookController {
 
         List<Book> books = bookService.getAllBooks();
 
-        List<BookResponseDto> list = new ArrayList<>();
+        List<BookResponseDtMZ> list = new ArrayList<>();
 
         for (Book book : books) {
             list.add(toResponseDto(book));
